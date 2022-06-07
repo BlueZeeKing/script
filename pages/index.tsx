dd076e9bbca6498bd1148aa9a94651b89d92e8d8
BlueZeeKing <github@blueish.dev>
2022-06-07 13:39:17 -0400

    setup

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Script App</title>
+        <meta name="description" content="Easily organize and display your lines for any script" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+
+      <footer>
+        <a href="https://github.com/BlueZeeKing">BlueZeeKing24</a>
+      </footer>
+    </div>
+  )
+}
